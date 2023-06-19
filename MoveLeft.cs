393be4cb3e739f4e5fa77c87653789278b3fc7cb
ArtCollision <candice@artcollision.ca>
2023-06-18 20:39:17 -0400

    Stop MoveLeft on gameOver
    I’ve added a gameOver bool that seems to work,  but the background and the objects continue to move when they collide with an obstacle. I need the MoveLeft script to communicate with the PlayerController, and stop once the player triggers gameOver.

--- MoveLeft.cs
+++ MoveLeft.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerControler PlayerControlerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerControlerScript =  GameObject.Find("Player").GetComponent<PlayerControler>();
     }
 
     // Update is called once per frame
