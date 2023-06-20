b979af16bd1f018b9c6e3a414bd051113ae0b593
ArtCollision <candice@artcollision.ca>
2023-06-20 14:13:45 -0400

    Set up a falling animation
    The running and jumping animations look great, but there’s one more state that the character should have an animation for. Instead of continuing to sprint when it collides with an object, the character should fall over as if it has been knocked out.

--- PlayerControler.cs
+++ PlayerControler.cs
@@ -40,7 +40,8 @@ public class PlayerControler : MonoBehaviour
         { 
             Debug.Log("Game Over");
             gameOver = true;
-            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
